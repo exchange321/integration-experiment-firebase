540f62cb2e5273555c3489e56f29f8601a3bea4a
exchange321 <chuen_hong@yahoo.com>
2017-02-11 15:40:15 +1000

    1. Initialize React Native 2. Separate WebApp and MobileApp instead of binding together

---
+++ WebApp/src/store/configureStore.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Wayuki on 03-Feb-17 0003.
+ */
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { browserHistory } from 'react-router';
+import { routerMiddleware } from 'react-router-redux';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+
+import rootReducer from '../reducers';
+import fbConfig from '../../../Shared/firebaseConfig';
+
+const configureStore = initialState => (
+    createStore(
+        rootReducer,
+        initialState,
+        compose(
+            applyMiddleware(thunk.withExtraArgument(getFirebase), routerMiddleware(browserHistory)),
+            reactReduxFirebase(fbConfig, { userProfile: 'users', enableLogging: false }),
+        ),
+    )
+);
+
+export default configureStore;
